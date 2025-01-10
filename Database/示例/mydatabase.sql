956e45268cd62afa47a4720e6da364dc25855c84
Chuwu <141227996+s1805948721@users.noreply.github.com>
2025-01-10 13:14:42 +0800

    Add files via upload

---
+++ Database/示例/mydatabase.sql
@@ -0,0 +1,272 @@
+/*
+Navicat MySQL Data Transfer
+
+Source Server         : MySQL80
+Source Server Version : 80033
+Source Host           : localhost:3306
+Source Database       : mydatabase
+
+Target Server Type    : MYSQL
+Target Server Version : 80033
+File Encoding         : 65001
+
+Date: 2025-01-10 13:14:22
+*/
+
+SET FOREIGN_KEY_CHECKS=0;
+
+-- ----------------------------
+-- Table structure for auth_group
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group`;
+CREATE TABLE `auth_group` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(150) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `name` (`name`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_group
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_group_permissions
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_group_permissions`;
+CREATE TABLE `auth_group_permissions` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `group_id` int NOT NULL,
+  `permission_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_group_permissions_group_id_permission_id_0cd325b0_uniq` (`group_id`,`permission_id`),
+  KEY `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_group_permissio_permission_id_84c5c92e_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_group_permissions_group_id_b120cbf9_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_group_permissions
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_permission
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_permission`;
+CREATE TABLE `auth_permission` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL,
+  `content_type_id` int NOT NULL,
+  `codename` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_permission_content_type_id_codename_01ab375a_uniq` (`content_type_id`,`codename`),
+  CONSTRAINT `auth_permission_content_type_id_2f476e4b_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_permission
+-- ----------------------------
+INSERT INTO `auth_permission` VALUES ('1', 'Can add log entry', '1', 'add_logentry');
+INSERT INTO `auth_permission` VALUES ('2', 'Can change log entry', '1', 'change_logentry');
+INSERT INTO `auth_permission` VALUES ('3', 'Can delete log entry', '1', 'delete_logentry');
+INSERT INTO `auth_permission` VALUES ('4', 'Can view log entry', '1', 'view_logentry');
+INSERT INTO `auth_permission` VALUES ('5', 'Can add permission', '2', 'add_permission');
+INSERT INTO `auth_permission` VALUES ('6', 'Can change permission', '2', 'change_permission');
+INSERT INTO `auth_permission` VALUES ('7', 'Can delete permission', '2', 'delete_permission');
+INSERT INTO `auth_permission` VALUES ('8', 'Can view permission', '2', 'view_permission');
+INSERT INTO `auth_permission` VALUES ('9', 'Can add group', '3', 'add_group');
+INSERT INTO `auth_permission` VALUES ('10', 'Can change group', '3', 'change_group');
+INSERT INTO `auth_permission` VALUES ('11', 'Can delete group', '3', 'delete_group');
+INSERT INTO `auth_permission` VALUES ('12', 'Can view group', '3', 'view_group');
+INSERT INTO `auth_permission` VALUES ('13', 'Can add user', '4', 'add_user');
+INSERT INTO `auth_permission` VALUES ('14', 'Can change user', '4', 'change_user');
+INSERT INTO `auth_permission` VALUES ('15', 'Can delete user', '4', 'delete_user');
+INSERT INTO `auth_permission` VALUES ('16', 'Can view user', '4', 'view_user');
+INSERT INTO `auth_permission` VALUES ('17', 'Can add content type', '5', 'add_contenttype');
+INSERT INTO `auth_permission` VALUES ('18', 'Can change content type', '5', 'change_contenttype');
+INSERT INTO `auth_permission` VALUES ('19', 'Can delete content type', '5', 'delete_contenttype');
+INSERT INTO `auth_permission` VALUES ('20', 'Can view content type', '5', 'view_contenttype');
+INSERT INTO `auth_permission` VALUES ('21', 'Can add session', '6', 'add_session');
+INSERT INTO `auth_permission` VALUES ('22', 'Can change session', '6', 'change_session');
+INSERT INTO `auth_permission` VALUES ('23', 'Can delete session', '6', 'delete_session');
+INSERT INTO `auth_permission` VALUES ('24', 'Can view session', '6', 'view_session');
+
+-- ----------------------------
+-- Table structure for auth_user
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user`;
+CREATE TABLE `auth_user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `password` varchar(128) NOT NULL,
+  `last_login` datetime(6) DEFAULT NULL,
+  `is_superuser` tinyint(1) NOT NULL,
+  `username` varchar(150) NOT NULL,
+  `first_name` varchar(150) NOT NULL,
+  `last_name` varchar(150) NOT NULL,
+  `email` varchar(254) NOT NULL,
+  `is_staff` tinyint(1) NOT NULL,
+  `is_active` tinyint(1) NOT NULL,
+  `date_joined` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=2 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_user
+-- ----------------------------
+INSERT INTO `auth_user` VALUES ('1', 'pbkdf2_sha256$870000$GvQtOUPgO8jd51w72oVBY5$YboELUFmb5yywbFw14ECOSIpYqKJECe85IsmbsJoBk0=', '2025-01-06 01:20:03.318064', '1', 'chuwu', '', '', '1805948721@qq.com', '1', '1', '2025-01-06 01:17:02.674035');
+
+-- ----------------------------
+-- Table structure for auth_user_groups
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user_groups`;
+CREATE TABLE `auth_user_groups` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `group_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_groups_user_id_group_id_94350c0c_uniq` (`user_id`,`group_id`),
+  KEY `auth_user_groups_group_id_97559544_fk_auth_group_id` (`group_id`),
+  CONSTRAINT `auth_user_groups_group_id_97559544_fk_auth_group_id` FOREIGN KEY (`group_id`) REFERENCES `auth_group` (`id`),
+  CONSTRAINT `auth_user_groups_user_id_6a12ed8b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_user_groups
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for auth_user_user_permissions
+-- ----------------------------
+DROP TABLE IF EXISTS `auth_user_user_permissions`;
+CREATE TABLE `auth_user_user_permissions` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `user_id` int NOT NULL,
+  `permission_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `auth_user_user_permissions_user_id_permission_id_14a6b632_uniq` (`user_id`,`permission_id`),
+  KEY `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` (`permission_id`),
+  CONSTRAINT `auth_user_user_permi_permission_id_1fbb5f2c_fk_auth_perm` FOREIGN KEY (`permission_id`) REFERENCES `auth_permission` (`id`),
+  CONSTRAINT `auth_user_user_permissions_user_id_a95ead1b_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of auth_user_user_permissions
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for django_admin_log
+-- ----------------------------
+DROP TABLE IF EXISTS `django_admin_log`;
+CREATE TABLE `django_admin_log` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `action_time` datetime(6) NOT NULL,
+  `object_id` longtext,
+  `object_repr` varchar(200) NOT NULL,
+  `action_flag` smallint unsigned NOT NULL,
+  `change_message` longtext NOT NULL,
+  `content_type_id` int DEFAULT NULL,
+  `user_id` int NOT NULL,
+  PRIMARY KEY (`id`),
+  KEY `django_admin_log_content_type_id_c4bce8eb_fk_django_co` (`content_type_id`),
+  KEY `django_admin_log_user_id_c564eba6_fk_auth_user_id` (`user_id`),
+  CONSTRAINT `django_admin_log_content_type_id_c4bce8eb_fk_django_co` FOREIGN KEY (`content_type_id`) REFERENCES `django_content_type` (`id`),
+  CONSTRAINT `django_admin_log_user_id_c564eba6_fk_auth_user_id` FOREIGN KEY (`user_id`) REFERENCES `auth_user` (`id`),
+  CONSTRAINT `django_admin_log_chk_1` CHECK ((`action_flag` >= 0))
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of django_admin_log
+-- ----------------------------
+
+-- ----------------------------
+-- Table structure for django_content_type
+-- ----------------------------
+DROP TABLE IF EXISTS `django_content_type`;
+CREATE TABLE `django_content_type` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `app_label` varchar(100) NOT NULL,
+  `model` varchar(100) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `django_content_type_app_label_model_76bd3d3b_uniq` (`app_label`,`model`)
+) ENGINE=InnoDB AUTO_INCREMENT=7 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of django_content_type
+-- ----------------------------
+INSERT INTO `django_content_type` VALUES ('1', 'admin', 'logentry');
+INSERT INTO `django_content_type` VALUES ('3', 'auth', 'group');
+INSERT INTO `django_content_type` VALUES ('2', 'auth', 'permission');
+INSERT INTO `django_content_type` VALUES ('4', 'auth', 'user');
+INSERT INTO `django_content_type` VALUES ('5', 'contenttypes', 'contenttype');
+INSERT INTO `django_content_type` VALUES ('6', 'sessions', 'session');
+
+-- ----------------------------
+-- Table structure for django_migrations
+-- ----------------------------
+DROP TABLE IF EXISTS `django_migrations`;
+CREATE TABLE `django_migrations` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `app` varchar(255) NOT NULL,
+  `name` varchar(255) NOT NULL,
+  `applied` datetime(6) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=19 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of django_migrations
+-- ----------------------------
+INSERT INTO `django_migrations` VALUES ('1', 'contenttypes', '0001_initial', '2025-01-06 01:13:54.287714');
+INSERT INTO `django_migrations` VALUES ('2', 'auth', '0001_initial', '2025-01-06 01:13:54.544153');
+INSERT INTO `django_migrations` VALUES ('3', 'admin', '0001_initial', '2025-01-06 01:13:54.611594');
+INSERT INTO `django_migrations` VALUES ('4', 'admin', '0002_logentry_remove_auto_add', '2025-01-06 01:13:54.616452');
+INSERT INTO `django_migrations` VALUES ('5', 'admin', '0003_logentry_add_action_flag_choices', '2025-01-06 01:13:54.621473');
+INSERT INTO `django_migrations` VALUES ('6', 'contenttypes', '0002_remove_content_type_name', '2025-01-06 01:13:54.657060');
+INSERT INTO `django_migrations` VALUES ('7', 'auth', '0002_alter_permission_name_max_length', '2025-01-06 01:13:54.684743');
+INSERT INTO `django_migrations` VALUES ('8', 'auth', '0003_alter_user_email_max_length', '2025-01-06 01:13:54.702677');
+INSERT INTO `django_migrations` VALUES ('9', 'auth', '0004_alter_user_username_opts', '2025-01-06 01:13:54.707677');
+INSERT INTO `django_migrations` VALUES ('10', 'auth', '0005_alter_user_last_login_null', '2025-01-06 01:13:54.733841');
+INSERT INTO `django_migrations` VALUES ('11', 'auth', '0006_require_contenttypes_0002', '2025-01-06 01:13:54.735869');
+INSERT INTO `django_migrations` VALUES ('12', 'auth', '0007_alter_validators_add_error_messages', '2025-01-06 01:13:54.740885');
+INSERT INTO `django_migrations` VALUES ('13', 'auth', '0008_alter_user_username_max_length', '2025-01-06 01:13:54.766972');
+INSERT INTO `django_migrations` VALUES ('14', 'auth', '0009_alter_user_last_name_max_length', '2025-01-06 01:13:54.793977');
+INSERT INTO `django_migrations` VALUES ('15', 'auth', '0010_alter_group_name_max_length', '2025-01-06 01:13:54.807932');
+INSERT INTO `django_migrations` VALUES ('16', 'auth', '0011_update_proxy_permissions', '2025-01-06 01:13:54.813525');
+INSERT INTO `django_migrations` VALUES ('17', 'auth', '0012_alter_user_first_name_max_length', '2025-01-06 01:13:54.842357');
+INSERT INTO `django_migrations` VALUES ('18', 'sessions', '0001_initial', '2025-01-06 01:13:54.859405');
+
+-- ----------------------------
+-- Table structure for django_session
+-- ----------------------------
+DROP TABLE IF EXISTS `django_session`;
+CREATE TABLE `django_session` (
+  `session_key` varchar(40) NOT NULL,
+  `session_data` longtext NOT NULL,
+  `expire_date` datetime(6) NOT NULL,
+  PRIMARY KEY (`session_key`),
+  KEY `django_session_expire_date_a5c62663` (`expire_date`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of django_session
+-- ----------------------------
+INSERT INTO `django_session` VALUES ('cj8awk36a319l2wy01w6cxom1rha52x8', 'eyJ1c2VybmFtZSI6ImNodXd1In0:1tUxeM:oA0BbV6L7lY9GBRg3ngb4kXkS3zmtyxr1d9TqgnS69Y', '2025-01-21 00:40:50.848949');
+INSERT INTO `django_session` VALUES ('jga2wtonazv69b03gvs7uc127owblfcl', '.eJxVjDEOAiEQRe9CbQgMAmJp7xnIwDCyaiBZdivj3XWTLbT9773_EhHXpcZ1lDlOJM5Ci8PvljA_StsA3bHdusy9LfOU5KbInQ557VSel939O6g46rdmBuUhI6kUOHv24E42FQ6OgYoujAGDQSDNzqpwVJqyNo7JgskUvHh_AAtUOJw:1tUyds:seX8vBehsTrpLi7RZxne2fBDNcU0HzjBON2-sgT8Jl8', '2025-01-21 01:44:24.150017');
+
+-- ----------------------------
+-- Table structure for user
+-- ----------------------------
+DROP TABLE IF EXISTS `user`;
+CREATE TABLE `user` (
+  `id` int NOT NULL AUTO_INCREMENT,
+  `username` varchar(50) NOT NULL,
+  `password` varchar(255) NOT NULL,
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `username` (`username`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+
+-- ----------------------------
+-- Records of user
+-- ----------------------------
+INSERT INTO `user` VALUES ('1', 'chuwu', 'youli20041030');
+INSERT INTO `user` VALUES ('2', 'ccxlb', 'ccxlb');
