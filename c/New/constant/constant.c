8f1d9f6c0cab9d2c420a5ad2cc0c2b5bd1c8d64e
HatsuChuwu <141227996+HatsuChuwu@users.noreply.github.com>
2025-02-18 20:54:12 +0800

    Add files via upload

---
+++ c/New/constant/constant.c
@@ -0,0 +1,9 @@
+#include<stdio.h>
+#include<stdlib.h>
+#define MAX_LEVEL 100
+int main() {
+    const double PI = 3.14; // PI is a constant
+    printf("PI 11= %f\n", PI); // PI cannot be changed
+    //PI = 3.1111;
+    printf("MAX_LEVEL = %d\n", MAX_LEVEL);
+}
