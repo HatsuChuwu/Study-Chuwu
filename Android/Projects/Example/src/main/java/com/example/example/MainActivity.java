ca72f8ba026951be800a54ddf210529a3d661bbd
HatsuChuwu <141227996+HatsuChuwu@users.noreply.github.com>
2025-04-01 23:21:09 +0800

    Add files via upload

---
+++ Android/Projects/Example/src/main/java/com/example/example/MainActivity.java
@@ -0,0 +1,43 @@
+package com.example.example;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.ImageView;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+public class MainActivity extends AppCompatActivity {
+    int num = 0;
+    ImageView img;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_main);
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
+            Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
+            v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
+            return insets;
+        });
+
+        img = findViewById(R.id.img);
+        img.setClickable(true);
+        img.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                num++;
+                if (num % 2 != 0) {
+                    img.setImageResource(R.drawable.off);
+                }else{
+                    img.setImageResource(R.drawable.on);
+                }
+            }
+        });
+
+    }
+}
